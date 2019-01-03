981a847bd4ff6fcb7ed37bb2f9c30254f9f1c39f
tylersassatelli <41389442+tylersassatelli@users.noreply.github.com>
2019-01-03 11:16:32 -0600

    webcam fun challenge complete

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,105 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false }) // this is how we get the user's video
+        .then(localMediaStream => {
+            console.log(localMediaStream);
+            video.src = window.URL.createObjectURL(localMediaStream); // takes video and converts it into something the video player can use
+            video.play();
+        })
+        .catch(err => {
+            console.error(`OH NO!!!`, err);
+        });
+}
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+        // take pixels out
+        let pixels = ctx.gitImageData(0, 0, width, height);
+        // change them using effect function
+
+        // red effect
+        // pixels = redEffect(pixels);
+
+        // rgb split effect
+        // pixels = rgbSplit(pixels);
+        // ctx.globalAlpha = 0.1; 
+
+        // green screen effects
+        pixels = greenScreen(pixels);
+
+        // put them back in
+        ctx.putImageData(pixels, 0, 0)
+    }, 16);
+}
+
+function takePhoto() {
+    // play sound
+    snap.currentTime = 0;
+    snap.play();
+
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+    strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i + 0] = pixels.data[i + 0] + 100; // red
+        pixels.data[i + 1] = pixels.data[i + 1] - 50; // green
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // blue
+    }
+    return pixels;
+}
+
+function rgbSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i - 150] = pixels.data[i + 0]; // red
+        pixels.data[i + 100] = pixels.data[i + 1]; // green
+        pixels.data[i - 150] = pixels.data[i + 2]; // blue
+    }
+    return pixels;
+}
+
+function greenScreen(pixels) {
+    const levels = {}; // holds our min and max levels
+
+    document.querySelectorAll('.rgb input').forEach((input) => {
+        levels[input.name] = input.value;
+    });
+
+    for (i = 0; i < pixels.data.length; i += 4) {
+        red = pixels.data[i + 0];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+        alpha = pixels.data[i + 3];
+
+        if (red >= levels.rmin
+            && green >= levels.gmin
+            && blue >= levels.bmin
+            && red <= levels.rmax
+            && green <= levels.gmax
+            && blue <= levels.bmax) {
+            // if pixels are in ranges above then 'take it out', or rather set its alpha to 0 or fully transparent
+            pixels.data[i + 3] = 0;
+        }
+    }
+    return pixels;
+}
+
+
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
